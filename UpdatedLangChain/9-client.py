1e7de8a44663bc8c0f8f22f37a5c49338e1063d5
Rohan-1103 <rohangoswami1134@gmail.com>
2026-06-01 01:02:47 +0530

    Completet MCP

--- UpdatedLangChain/9-client.py
+++ UpdatedLangChain/9-client.py
@@ -3,38 +3,71 @@ from langchain.agents import create_agent
 from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
-load_dotenv()
-
 import asyncio
-import sys
 import os
+
+# Load .env
+load_dotenv()
 
 
 async def main():
-    repo_dir = os.path.dirname(__file__)
-    math_script = os.path.join(repo_dir, "7-mathserver.py")
+
+    # Get API Key
+    groq_key = os.getenv("GROQ_API_KEY")
+
+    if not groq_key:
+        raise ValueError("GROQ_API_KEY not found in .env file")
+
+    os.environ["GROQ_API_KEY"] = groq_key
+
+    # MCP Client
     client = MultiServerMCPClient(
         {
-            "math":{
-                "command": sys.executable,
-                "args":[math_script],
+            # Math MCP Server (STDIO)
+            "math": {
+                "command": "python",
+                "args": ["7-mathserver.py"],
                 "transport": "stdio",
             },
-            "weather":{
+
+            # Weather MCP Server (HTTP)
+            "weather": {
                 "url": "http://127.0.0.1:8000/mcp",
-                "transport": "streamable_http",  
-            }       
+                "transport": "streamable_http",
+            }
         }
     )
-    
-    os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-    
-    tools = await client.get_tools()
-    model = ChatGroq(model="llama-3.1-8b-instant", temperature=0.0)
-    agent = create_agent(model=model, tools=tools)
-    
-    math_response = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "What is (3 + 5) x 12?"}]})
-    print("Math response: ", math_response['messages'][-1].content)
 
+    # Load tools from MCP servers
+    tools = await client.get_tools()
+
+    # LLM
+    model = ChatGroq(
+        model="llama-3.1-8b-instant"
+    )
+
+    # Create Agent
+    agent = create_agent(
+        model=model,
+        tools=tools
+    )
+
+    # Query
+    response = await agent.ainvoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "What is the weather in Mumbai and what is (3 + 5) x 12?"
+                }
+            ]
+        }
+    )
+
+    # Print final answer
+    print("\nFinal Response:\n")
+    print(response["messages"][-1].content)
+
+
+# Run app
 asyncio.run(main())
